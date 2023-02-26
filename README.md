cb3976084db952ca7775d6522db3e6c3361aa78b
kallu <petri.kallberg@gmail.com>
2023-02-26 07:35:09 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # aws-taggable-services
-Lists of AWS services based on if tagging is supported or not
+Lists of AWS services based on if tagging is supported or not.
+
+* `all-services-n-actions.txt` list of all AWS services and API actions, in SERVICE:ACTION -format.
+* `all-services.txt` list of all AWS services.
+* `taggable-services.txt` list of AWS services that have tagging support.
+* `untaggable-services.txt` list of AWS services that don't have tagging support.
+* `update.sh` script to create above file automatically.
+
+
---
+++ update.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Get a list of all AWS services API actions and create
+# lists of services that do or do not, support tagging.
+
+# Dependencies
+
+CURL=curl # https://curl.se/
+JQ=jq	  # https://stedolan.github.io/jq/
+
+# Get the list of all services and actions in format SERVICE:ACTION
+# This is assuming every service has an API and IAM integration.
+$CURL --header 'Connection: keep-alive' \
+     --header 'Pragma: no-cache' \
+     --header 'Cache-Control: no-cache' \
+     --header 'Accept: */*' \
+     --header 'Referer: https://awspolicygen.s3.amazonaws.com/policygen.html' \
+     --header 'Accept-Language: en-US,en;q=0.9' \
+     --silent \
+     --compressed \
+     'https://awspolicygen.s3.amazonaws.com/js/policies.js' |
+    cut -d= -f2 |
+    $JQ -r '.serviceMap[] | .StringPrefix as $prefix | .Actions[] | "\($prefix):\(.)"' |
+    sort |
+    uniq > all-services-n-actions.txt
+
+# Create a list of all services
+cat all-services-n-actions.txt | sed 's/\:.*$//g' | sort | uniq > all-services.txt
+
+# Create a list of services that support tagging
+# ie. services that have an action with string Tag in it
+cat all-services-n-actions.txt | grep Tag | sed 's/\:.*$//g' | sort | uniq > taggable-services.txt
+
+# Create a list services that DONT supporting tagging
+# ie. lines that are not present in both all-services.txt and taggable-services.txt
+cat all-services.txt  taggable-services.txt | sort | uniq --unique > untaggable-services.txt
+
